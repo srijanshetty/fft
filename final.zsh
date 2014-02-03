1c6a676b1df4c74f67e3e844c5b4cd25f02bfd4b
Srijan R Shetty <srijan.shetty@gmail.com>
2014-02-03 15:15:52 +0530

    final updates

--- final.zsh
+++ final.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-for i in {1..$1};do
+for i in {18..$1};do
     lim=$(( 2**$i ))
     echo -n "$lim "
-    ./create.zsh 10 $lim "$3" && "./$2" < "$3" 
+    ./create.zsh 1 $lim "$3" && "./$2" < "$3" 
 done
